570d0652853d4eed22e625819ca6dc0dbe945c97
Gabriel de Oliveira Ramalho <89716818+Gabrieldor@users.noreply.github.com>
2024-10-22 12:22:38 -0300

    Update testbizapedia.py

--- testbizapedia.py
+++ testbizapedia.py
@@ -3,7 +3,6 @@ import json
 
 
 def quote():
-    with open("json.json", 'r') as f:
         r = requests.post("http://localhost:8000/api/v1/bizapedia/quote", json={"state_code" : "Mississippi", "company_name": "CURTIS AND INGRAM TRUCKING LLC"})
         print(r.content)
             
